21f0e86031ba9fd8d5769e8c96d7f8cdaf2ada44
djcortez <djcortez@gmail.com>
2023-04-26 20:13:07 -0600

    Terminando peticiones http

--- 07-http-app/components/meal/meal-app.js
+++ 07-http-app/components/meal/meal-app.js
@@ -4,22 +4,56 @@
  */
 export const MealAppComponent = async(containerElement) => {
 
-    console.log('enter to component 33');
+    console.log('enter to component 336');
     containerElement.innerText = 'Loading from API....';
 
-    fetchMeal();
+    const titleElement = document.createElement('h4');    
+    const imgElement = document.createElement('img');
+    const btnElement = document.createElement('button');
+    btnElement.innerText = 'Next meal';
+
+    const renderData = (data) => {
+        titleElement.innerText = data.strMeal;        
+        imgElement.src = data.strMealThumb;
+
+        containerElement.replaceChildren(titleElement, imgElement, btnElement);
+    };
+
+    renderData(await fetchMealData());
+
+    btnElement.addEventListener('click', ()=> {
+
+        containerElement.innerHTML = 'Loading meal....';
+        
+        setTimeout(async() => {
+            renderData(await fetchMealData());   
+        }, 1000);          
+    });
+    
 }
 
-const fetchMeal = async() => {
+const fetchMealData = async() => {
 
     let response;
     try {
         response = await fetch(
-            import.meta.env.VITE_API_URL_V2 + '&apiKey=' +
-            import.meta.env.VITE_API_KEY);
+           /* import.meta.env.VITE_API_URL_V2 + '&apiKey=' +
+            import.meta.env.VITE_API_KEY*/
+            import.meta.env.VITE_API_URL);
     } catch (err) {
         alert(err);
     }
 
-    console.log(response);
+    const data = await response.json();
+    console.log(data.meals[0]);
+
+    return data.meals[0];
+}
+
+const fetchMeal = async() => {
+
+    const response = await fetch(import.meta.env.VITE_API_URL);
+    const data = await response.json();
+
+    return data;
 }
